6a5053410d704092a785ea9c3b6dc3037eeb40d4
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 04:08:18 +0600

    final commit

---
+++ switch.js
@@ -0,0 +1,16 @@
+var n = 23;
+switch (n) {
+    case 20:
+        console.log("Tweenty");
+        break;
+    case 23:
+        console.log('Tweenty Three');
+        break;
+    case 5:
+    case 10:
+        console.log('5 or 10');
+        break;
+    default:
+        console.log("Dhet");
+        break;
+}
