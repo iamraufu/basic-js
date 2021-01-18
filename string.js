ea900e88ba56c9c10f86829d60aceb7060d0ba5b
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 00:51:51 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,11 @@
+var promise = "I promise myself to work hard and become a web developer";
+promise.toLowerCase();
+console.log(promise.toLowerCase());
+
+promise.toUpperCase;
+console.log(promise.toUpperCase());
+
+console.log(promise.indexOf('promise'));
+
+console.log(promise.split(''));
+console.log(promise.split(' '));
