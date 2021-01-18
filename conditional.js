cfde7e9654f1d60dd49a63ca553c167eb1ef818f
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 02:17:47 +0600

    first commit

---
+++ conditional.js
@@ -0,0 +1,8 @@
+var job = true;
+var salary = 50000;
+var savings = 600000;
+if (job == true || salary >= 50000 || savings > 500000) {
+    console.log('Lets find a girl for you to marry');
+} else {
+    console.log('You will die single');
+}
