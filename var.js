98e665d20ba6024184d76f6a13d63618bc91b79d
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-18 13:13:35 +0600

    first commit

--- var.js
+++ var.js
@@ -1,2 +1,11 @@
 var price = 120;
 console.log(price);
+
+var sup = 2;
+console.log(typeof sup);
+
+var name = "Raufu Prezens";
+console.log(typeof name);
+
+var isHot = false;
+console.log(typeof isHot);
