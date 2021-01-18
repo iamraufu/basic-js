5389a043314eebd92ad700dc25988657f2807c88
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 04:25:15 +0600

    final commit

--- function.js
+++ function.js
@@ -3,3 +3,16 @@ function sayHello() {
 }
 
 sayHello();
+
+function doubleNum(num1, num2) {
+    var result = num1 + num2;
+    return result;
+
+}
+var f1 = doubleNum(2);
+var f2 = doubleNum(3);
+var total = f1 + f2;
+console.log(total);
+
+var sum = doubleNum(5, 3);
+console.log(sum);
