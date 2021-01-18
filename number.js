dfe9bf169f96a7639752b7b745aee5eb203a247c
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 01:10:19 +0600

    first commit

---
+++ number.js
@@ -0,0 +1,13 @@
+var n1 = 25;
+var n2 = 10.5;
+var s1 = '32';
+
+console.log(n1 + n2);
+
+total = n1 + n2;
+
+console.log(parseInt(total));
+
+console.log(parseFloat(total));
+
+console.log(parseInt(s1));
