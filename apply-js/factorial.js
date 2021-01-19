7db410eda61af555fc4459bc2bd9b1daa56a1943
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 22:03:01 +0600

    final commit

--- apply-js/factorial.js
+++ apply-js/factorial.js
@@ -22,3 +22,15 @@ function whileFactorial(n) {
 }
 let fa = whileFactorial(4);
 console.log(fa);
+
+// recursion
+function factorialize(num) {
+    if (num < 0)
+        return -1;
+    else if (num == 0)
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+}
+console.log(factorialize(5));
