97e2af1b28eca1be490aa433d7e4e5879ce770a3
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-01-19 12:35:31 +0600

    first commit

---
+++ apply-js/constLet.js
@@ -0,0 +1,14 @@
+var name = 'Raufu';
+if (name.length > 4) {
+    name = 'Raufu Prezens';
+}
+console.log(name);
+
+// Const use for not changing again - it can't be change later
+const country = 'Bangladesh';
+
+// let use for changing later -it could be changed later
+let home = 'Rampura';
+console.log(country);
+home = 'Bonosree';
+console.log(home);
